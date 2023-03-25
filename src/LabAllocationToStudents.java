4a0a09ca0f3191172297acfb8ae4fd4c1dff2318
KaveyaAnnadurai <kaveya.a@m2pfintech.com>
2023-03-25 15:15:28 +0530

    Add. Included the logic.

--- src/LabAllocationToStudents.java
+++ src/LabAllocationToStudents.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class LabAllocationToStudents {
     public static void main(String[] args) {
+        int n, minimumDifference = Integer.MAX_VALUE,correctLab=0;
+        int[] noOfSystems = new int[3];
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello world!");
+        System.out.println("Enter systems in lab1:");
+        noOfSystems[0] = scanner.nextInt();
+        System.out.println("Enter systems in lab2:");
+        noOfSystems[1] = scanner.nextInt();
+        System.out.println("Enter systems in lab3:");
+        noOfSystems[2] = scanner.nextInt();
+        System.out.println("Enter number of students:");
+        n=scanner.nextInt();
+
+        for(int iterator : noOfSystems){
+            if(iterator > n && minimumDifference > (iterator - n)){
+                minimumDifference = iterator - n;
+                correctLab = iterator;
+            }
+        }
+        if(correctLab == noOfSystems[0])
+            System.out.println("The correct lab is L1");
+        else if (correctLab == noOfSystems[1])
+            System.out.println("The correct lab is L2");
+        else System.out.println("The correct lab is L3");
+
     }
 }
